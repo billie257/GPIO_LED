dab0701f1b6eb1e566a6191bb8681b44bc9a879f
liyuan <huanglyuan15@126.com>
2026-05-24 10:53:35 +0800

    feat: add led wave

--- app/main.c
+++ app/main.c
@@ -2,7 +2,7 @@
 #include "stm32f4xx.h"
 #include "led.h"
 
-#define PERIOD 1000
+#define PERIOD 100
 
 void cpu_delay(void)
 {
@@ -25,19 +25,27 @@ int main(void)
 {
 		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);
 		
-		led_init();
-	
+		led_init();	
 		led_all_off();
 	
+		int led1_duty = 0.2*PERIOD, led1_inc = 1;
+		int led2_duty = 0.8*PERIOD, led2_inc = 1;	
+	
 		while(1)
-		{			
-			for (int d = 0; d < PERIOD; d++)
-			{			
-					led_breath(d, 50);
-			}	
-			for (int d = PERIOD; d > 0; d--)
-			{
-					led_breath(d, 50);
-			}			
+		{	
+				for (int t = 0; t < 100; t++)
+				{
+					for (int i = 0; i < PERIOD; i++)
+					{			
+							if (i < led1_duty)  led_on(1); else led_off(1);
+							if (i < led2_duty)  led_on(2); else led_off(2);						
+					}			
+				}	
+				led1_duty += led1_inc; 
+				led2_duty += led2_inc; 			
+				
+				if (led1_duty >= PERIOD || led1_duty <= 0) led1_inc = -led1_inc;
+				if (led2_duty >= PERIOD || led2_duty <= 0) led2_inc = -led2_inc;
 		}
+		
 }
